02e18f358658f86e15d15931030e73b69c3c45a2
Audrey <andreatumdrey@gmail.com>
2025-05-31 14:49:01 -0700

    basic javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "Audrey";
+let age = "14";
+let likesSushi = false;
+
+function greeting(name) {
+    console.log("Hello", name);
+}
+
+greeting("Audrey");
+
+let newName = document.getElementById("greeter").value;
+
