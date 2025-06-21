05393e28db09a851aa480c7cab69f1769d57e576
Audrey <andreatumdrey@gmail.com>
2025-06-21 11:53:07 -0700

    added password thing

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ document.querySelectorAll('.accordion-header').forEach(function (button){
         content.style.display = (content.style.display === 'block') ? 'none': 'block';
     });
 });
+
+const PASSWORD = "Summer2025";
+
+
+function validatePass() {
+    const login = document.getElementById('password');
+    const message = document.getElementById('message');
+    const hiddenEntries =document.querySelectorAll('.hidden');
+
+    
+    
+
+    if(login.value === PASSWORD) {
+        message.textContent = "Password Confirmed. See hidden entries below.";
+        hiddenEntries.forEach(function(entry) {entry.style.display = 'flex';});
+    } else {
+        message.textContent = "Password Denied. Try again.";
+        hiddenEntries.forEach(function(entry) {entry.style.display = 'none';});
+    }
+
+    login.value = '';
+}
+
